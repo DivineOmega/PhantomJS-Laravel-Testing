98c4f3cb4859433cbfbeac08c2021f2125ec97d4
Jordan Hall <jordan.hall@rapidweb.biz>
2017-02-10 10:24:19 +0000

    Change waitFor argument to proper Closure

--- src/Traits/CrawlerTrait.php
+++ src/Traits/CrawlerTrait.php
@@ -35,7 +35,9 @@ trait CrawlerTrait
 
     public function waitToSee($string, $timeout = 5) 
     {
-        $stringPresentInSource = $this->waitFor($this->isStringPresentInSource($string));
+        $stringPresentInSource = $this->waitFor(function() {
+            return $this->isStringPresentInSource($string);
+        });
 
         $this->assertTrue($stringPresentInSource, 'Could not find \''.$string.'\' in page source code: '.print_r($source, true));
     }
